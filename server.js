1b597da32c0ebfbdca374252b16ce3268c1b89bb
mykhailo.zheliaskov <mykhailo.zheliaskov@decerto.pl>
2020-11-08 13:00:38 +0100

    Build fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app.js');
 const port = 3000;
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(process.env.PORT || port, '0.0.0.0', () => {
 	console.log(`App started on port: ${port}`);
 })
